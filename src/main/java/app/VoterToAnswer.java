652b755dea51801702cb69af0462f41ad90a4d47
Kirsi Suoranta <suorantakirsi@gmail.com>
2021-04-30 13:35:45 +0300

    PrimaryKeys etc

---
+++ src/main/java/app/VoterToAnswer.java
@@ -0,0 +1,43 @@
+package app;
+
+import java.io.*;
+import java.util.*;
+
+import javax.persistence.*;
+
+public class VoterToAnswer implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+//	private ArrayList<Integer> answer = new ArrayList<>(20);
+//	ArrayList<Tuple<Integer, Integer>> points = new ArrayList<>(20); Trying to understand Tuple, not working
+//	
+//	public void answersAndPoints() {
+//		for (int i = 0; i < 20; i++) {
+//			this.answer.add(0);
+//			this.points.add(new Tuple<>(0,0));
+//		}
+//	}
+//	
+//	public ArrayList<Integer> getAnswers() {
+//		return this.answer;
+//	}
+//	
+//	public Integer getPoints(int ehdokas_id) {
+//		if (this.points.size() >= ehdokas_id) {
+//			return this.points.get(ehdokas_id).points;
+//		} else {
+//			return 0;
+//		}
+//	}
+//	
+//	public void addPoints(Integer ehdokas_id, Integer points) {
+//		this.points.set(ehdokas_id, new Tuple<>(ehdokas_id, points));
+//	}
+//	
+//	public Integer getAnswer(int index) {
+//		return this.answer.get(index);
+//	}
+	
+	
+}
